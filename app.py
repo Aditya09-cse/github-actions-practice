bee07dedd8257230af25293acc74c1fd632db434
Aditya09-cse <deveshtomart18@gmail.com>
2026-04-11 23:58:03 +0530

    fix: linting for app.py

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+
 @app.route('/health')
 def health():
     return 'Server is up and running smoothly'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
